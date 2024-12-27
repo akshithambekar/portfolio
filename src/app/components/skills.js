b4a3dbf2962bff84b299db1ff04363c39688dee6
Akshith Ambekar <123666607+akshithambekar@users.noreply.github.com>
2024-12-26 22:51:21 -0500

    Completed technical skills, updated project display

--- src/app/components/skills.js
+++ src/app/components/skills.js
@@ -1,25 +1,34 @@
 "use client";
 
-import { useState } from "react";
-import Cursor from "./cursor";
+import { useState, useEffect } from "react";
 
 export default function Skills() {
     const [activeCategory, setActiveCategory] = useState();
     const categories = {
-        "languages": ["Python", "JavaScript", "TypeScript", "C++", "Java", "HTML/CSS", "MATLAB"],
-        "tools_frameworks": ["Next.js", "Express.js", "Node.js", "React.js", "Flask", "Tailwind", "Git", "OpenCV", "NumPy"]
+        "languages": [{ name: "Python", years: "5 years" }, { name: "Java", years: "5 years" }, { name: "HTML/CSS", years: "4 years" }, { name: "JavaScript", years: "3 years" }, { name: "C++", years: "2 years" }, { name: "MATLAB", years: "<1 year" }],
+        "tools_frameworks": [{ name: "Express.js", years: "2 years" }, { name: "Node.js", years: "2 years" }, { name: "Flask", years: "2 years" }, { name: "NumPy", years: "2 years" }, { name: "React", years: "1 year" }, { name: "OpenCV", years: "1 year" }, { name: "Next.js", years: "<1 year" }, { name: "Tailwind CSS", years: "<1 year" }]
     };
-    
+    useEffect(() => {
+        setActiveCategory("languages");
+    }, []);
     return (
         <div className="max-w-[1260px] justify-center items-center h-48 px-44 mx-auto">
-            <h1 className="text-custom_gray text-2xl text-center font-bold underline">Technical Skills</h1>
-            <div className="flex items-center justify-center py-6 space-x-4">
+            <h1 className="text-white text-2xl text-center font-bold underline">Technical Skills</h1>
+            <div className="flex items-center justify-center py-5 space-x-4">
                 <button onClick={() => setActiveCategory("languages")} className={`font-semibold px-4 py-2 rounded-full transition-colors ${activeCategory === "languages" ? "bg-gray-800 text-white" : "text-custom_gray hover:text-white"}`}>
                     Languages
                 </button>
                 <button onClick={() => setActiveCategory("tools_frameworks")} className={`font-semibold px-4 py-2 rounded-full transition-colors ${activeCategory === "tools_frameworks" ? "bg-gray-800 text-white" : "text-custom_gray hover:text-white"}`}>
                     Tools and Frameworks
                 </button>
+            </div>
+            <div className="flex flex-col justify-center text-custom_gray max-w-[640px] mx-auto pt-4">
+                {categories[activeCategory]?.map((item, index) => (
+                    <div key={index} className="flex justify-between items-center py-1">
+                        <p className="order-first text-lg font-semibold">{item.name}</p>
+                        <p className="order-last text-sm">{item.years}</p>
+                    </div>
+                ))}
             </div>
         </div>
     );
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -11,9 +11,23 @@ export default function Projects() {
             <div className="grid grid-flow-row text-custom_gray items-baseline h-48 subpixel-antialiased max-w-[1260px] px-44 mx-auto">
                 <div className="flex flex-row items-start justify-between">
                     <div className="max-w-[560px]">
-                        <div className="flex max-w-[373px] items-baseline">
-                            <Link href="https://github.com/akshithambekar/parkit" target="_blank" className="text-lg font-semibold underline decoration-1 px-4 hover:text-white">ParkIt</Link>
-                            <h6 className="text-base">JavaScript, Node.js, Express.js, HTML/CSS</h6>
+                        <div className="flex max-w-[560px] items-center">
+                            <Link href="https://github.com/akshithambekar/parkit" target="_blank" className="text-lg font-semibold underline decoration-1 px-4 text-gray-100 hover:text-white">ParkIt</Link>
+                            <p className="pr-4 text-sm">February 2023</p>
+                            <div className="flex flex-row space-x-2">
+                                <p className="px-3 py-1 text-xs rounded-full transition-colors bg-gray-800 text-custom_gray">
+                                    JavaScript
+                                </p>
+                                <p className="px-3 py-1 text-xs rounded-full transition-colors bg-gray-800 text-custom_gray">
+                                    Node.js
+                                </p>
+                                <p className="px-3 py-1 text-xs rounded-full transition-colors bg-gray-800 text-custom_gray">
+                                    Express.js
+                                </p>
+                                <p className="px-3 py-1 text-xs rounded-full transition-colors bg-gray-800 text-custom_gray">
+                                    HTML/CSS
+                                </p>
+                            </div>
                         </div>
                         <p className="text-sm px-4 mt-2">
                             Developed at HackTJ 10.0, ParkIt is a web application designed to help users find parking in congested areas. Using a live Google Maps interface, this app integrates the
@@ -24,7 +38,7 @@ export default function Projects() {
                     </div>
                     <div className="flex items-center justify-center px-8">
                         <Link href="https://youtu.be/fvdOhyDX81I" target="_blank">
-                            <Image src="/parkit-demo.png" alt="ParkIt demo" width={500} height={300} className="hover:opacity-75 object-contain shadow-2xl" />
+                            <Image src="/parkit-demo.png" alt="ParkIt demo" width={500} height={300} className="hover:opacity-75 object-contain shadow-2xl"/>
                         </Link>
                     </div>
                 </div>
