a548ef4377e567b339b2c75a10305f31a7903994
Akshith Ambekar <123666607+akshithambekar@users.noreply.github.com>
2024-12-31 13:28:06 -0500

    experience section completed

--- src/app/components/experience.js
+++ src/app/components/experience.js
@@ -1,67 +1,143 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { BsCalendar } from "react-icons/bs";
+import { BsCalendar, BsArrowRight } from "react-icons/bs";
 // https://react-icons.github.io/react-icons/icons/bs/
 
 const EducationSection = () => {
     return (
         <div className="flex flex-col items-center justify-center py-5 space-x-4">
-            <p class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-            </p>
-            <p class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-            </p>
+            <ol className="relative border-s border-gray-200 dark:border-gray-700">
+                <li className="mb-10 ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <div className="flex flex-row items-center">
+                        <BsCalendar className="text-gray-500" />
+                        <time className="pl-2 text-sm font-normal text-gray-400 dark:text-gray-500">
+                            May 2027
+                        </time>
+                    </div>
+                    <h3 className="text-lg pt-1 font-semibold text-gray-900 dark:text-white">
+                        George Mason University
+                    </h3>
+                    <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                        BS in Computer Science
+                    </p>
+                </li>
+                <li className="ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <div className="flex flex-row items-center">
+                        <BsCalendar className="text-gray-500" />
+                        <time className="pl-2 text-sm font-normal text-gray-400 dark:text-gray-500">
+                            June 2024
+                        </time>
+                    </div>
+                    <h3 className="text-lg pt-1 font-semibold text-gray-900 dark:text-white">
+                        Thomas Jefferson High School for Science and Technology
+                    </h3>
+                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                        Advanced Studies Diploma
+                    </p>
+                </li>
+            </ol>
         </div>
-    )
-}
+    );
+};
 
 const WorkSection = () => {
     return (
-        <div>
-
+        <div className="flex flex-col items-center justify-center py-5 space-x-4">
+            <ol className="relative border-s border-gray-200 dark:border-gray-700">
+                <li className="mb-10 ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <div className="flex flex-row items-center">
+                        <BsCalendar className="text-gray-500" />
+                        <div className="flex flex-row items-center pl-2 text-sm font-normal text-gray-400 dark:text-gray-500">
+                            <span>October 2024</span>
+                            <BsArrowRight className="mx-1" />
+                            <span>Present</span>
+                        </div>
+                    </div>
+                    <h3 className="text-lg pt-1 font-semibold text-gray-900 dark:text-white">
+                        Undergraduate Research Assistant
+                    </h3>
+                    <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                        George Mason University
+                    </p>
+                </li>
+                <li className="mb-10 ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <div className="flex flex-row items-center">
+                        <BsCalendar className="text-gray-500" />
+                        <div className="flex flex-row items-center pl-2 text-sm font-normal text-gray-400 dark:text-gray-500">
+                            <span>June 2024</span>
+                            <BsArrowRight className="mx-1" />
+                            <span>August 2024</span>
+                        </div>
+                    </div>
+                    <h3 className="text-lg pt-1 font-semibold text-gray-900 dark:text-white">
+                        Mathematics and Physics Tutor
+                    </h3>
+                    <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                        Coherence Learning Services
+                    </p>
+                </li>
+                <li className="ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <div className="flex flex-row items-center">
+                        <BsCalendar className="text-gray-500" />
+                        <div className="flex flex-row items-center pl-2 text-sm font-normal text-gray-400 dark:text-gray-500">
+                            <span>June 2023</span>
+                            <BsArrowRight className="mx-1" />
+                            <span>August 2023</span>
+                        </div>
+                    </div>
+                    <h3 className="text-lg pt-1 font-semibold text-gray-900 dark:text-white">
+                        Bioinformatics Research Intern
+                    </h3>
+                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                        Dartmouth-Hitchcock Medical Center
+                    </p>
+                </li>
+            </ol>
         </div>
-    )
-}
+    );
+};
 
 export default function Experience() {
     const [activeCategory, setActiveCategory] = useState();
-    const categories = ["education", "work"]
+    const categories = ["education", "work"];
     useEffect(() => {
         setActiveCategory("education");
     }, []);
     return (
-        <div className="max-w-[1260px] justify-center items-center h-[1024px] px-44 pt-4 subpixel-antialiased mx-auto">
-            <h1 className="text-white text-2xl text-center font-bold underline">Experience</h1>
+        <div className="max-w-[1260px] justify-center items-center h-[560px] px-44 pt-12 subpixel-antialiased mx-auto">
+            <h1 className="text-white text-2xl text-center font-bold underline">
+                Experience
+            </h1>
             <div className="flex items-center justify-center py-5 space-x-8">
-                <button onClick={() => setActiveCategory("education")} className={`font-semibold px-4 py-2 rounded-full transition-colors ${activeCategory === "education" ? "bg-gray-800 text-white" : "text-custom_gray hover:text-white"}`}>
+                <button
+                    onClick={() => setActiveCategory("education")}
+                    className={`font-semibold px-4 py-2 rounded-full transition-colors ${
+                        activeCategory === "education"
+                            ? "bg-gray-800 text-white"
+                            : "text-custom_gray hover:text-white"
+                    }`}
+                >
                     Education
                 </button>
-                <button onClick={() => setActiveCategory("work")} className={`font-semibold px-4 py-2 rounded-full transition-colors ${activeCategory === "work" ? "bg-gray-800 text-white" : "text-custom_gray hover:text-white"}`}>
+                <button
+                    onClick={() => setActiveCategory("work")}
+                    className={`font-semibold px-4 py-2 rounded-full transition-colors ${
+                        activeCategory === "work"
+                            ? "bg-gray-800 text-white"
+                            : "text-custom_gray hover:text-white"
+                    }`}
+                >
                     Work
                 </button>
             </div>
-            <div className="flex flex-row items-center justify-center py-4 space-x-4 space-y-4">
-                <div className="block max-w-sm px-5 py-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">George Mason University</h5>
-                    <p className="font-normal text-custom_gray pb-1">BS in Computer Science</p>
-                    <div className="flex flex-row justify-middle items-center text-center">
-                        <BsCalendar className="text-gray-500 py-0.25" />
-                        <p className="text-gray-500 pl-2 align-bottom">May 2027</p>
-                    </div>
-                </div>
-                <div className="block max-w-sm px-5 py-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">Thomas Jefferson High School for Science and Technology</h5>
-                    <p className="font-normal text-custom_gray pb-1">Advanced Studies Diploma</p>
-                    <div className="flex flex-row justify-middle items-center text-center">
-                        <BsCalendar className="text-gray-500 py-0.25" />
-                        <p className="text-gray-500 pl-2 align-bottom">June 2024</p>
-                    </div>
-                </div>
-            </div>
+            {activeCategory === "education" && <EducationSection />}
+            {activeCategory === "work" && <WorkSection />}
         </div>
     );
 }
--- src/app/components/skills.js
+++ src/app/components/skills.js
@@ -5,14 +5,14 @@ import { useState, useEffect } from "react";
 export default function Skills() {
     const [activeCategory, setActiveCategory] = useState();
     const categories = {
-        "languages": [{ name: "Python", years: "5 years" }, { name: "Java", years: "5 years" }, { name: "HTML/CSS", years: "4 years" }, { name: "JavaScript", years: "3 years" }, { name: "C++", years: "2 years" }, { name: "MATLAB", years: "<1 year" }],
-        "tools_frameworks": [{ name: "Express.js", years: "2 years" }, { name: "Node.js", years: "2 years" }, { name: "Flask", years: "2 years" }, { name: "NumPy", years: "2 years" }, { name: "React", years: "1 year" }, { name: "OpenCV", years: "1 year" }, { name: "Next.js", years: "<1 year" }, { name: "Tailwind CSS", years: "<1 year" }]
+        "languages": [{ name: "Python", years: "5 years" }, { name: "Java", years: "5 years" }, { name: "HTML/CSS", years: "4 years" }, { name: "JavaScript/TypeScript", years: "3 years" }, { name: "C++", years: "2 years" }, { name: "MATLAB", years: "<1 year" }],
+        "tools_frameworks": [{ name: "Express.js", years: "2 years" }, { name: "Node.js", years: "2 years" }, { name: "Flask", years: "2 years" }, { name: "NumPy", years: "2 years" }, { name: "React.js", years: "1 year" }, { name: "OpenCV", years: "1 year" }, { name: "Next.js", years: "<1 year" }, { name: "Tailwind CSS", years: "<1 year" }]
     };
     useEffect(() => {
         setActiveCategory("languages");
     }, []);
     return (
-        <div className="max-w-[1260px] justify-center items-center h-48 px-44 pt-4 subpixel-antialiased mx-auto">
+        <div className="max-w-[1260px] justify-center items-center h-32 px-44 pt-4 subpixel-antialiased mx-auto">
             <h1 className="text-white text-2xl text-center font-bold underline">Technical Skills</h1>
             <div className="flex items-center justify-center py-5 space-x-4">
                 <button onClick={() => setActiveCategory("languages")} className={`font-semibold px-4 py-2 rounded-full transition-colors ${activeCategory === "languages" ? "bg-gray-800 text-white" : "text-custom_gray hover:text-white"}`}>
