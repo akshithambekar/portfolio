b0f4c12801783a0994e05d5087626e3559b367b0
Akshith Ambekar <123666607+akshithambekar@users.noreply.github.com>
2025-02-05 22:26:24 -0500

    Refinements

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter_Tight, Archivo, DM_Sans} from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter_tight = Inter_Tight({
   subsets: ["latin"],
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter_tight.className} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
