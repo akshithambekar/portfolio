c3ef949ca79b17bea3f5d69659e468245605dee1
Akshith Ambekar <123666607+akshithambekar@users.noreply.github.com>
2025-12-08 09:19:54 -0500

    create base for new portfolio

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,13 @@
-import Image from "next/image";
 import Navbar from "./components/navbar";
-import Cursor from "./components/cursor";
-import Intro from "./components/intro";
-import Experience from "./components/experience";
-import Skills from "./components/skills";
-import Contact from "./components/contact";
 import Footer from "./components/footer";
 
 export default function Home() {
-  return (
-    <main className="subpixel-antialiased">
-      <Cursor />
-      <div>
-        <Navbar />
-        <Intro />
-        <Experience />
-        <Skills />
-        <Contact />
-        <Footer />
-      </div>
-    </main>
-  );
+    return (
+        <main className="subpixel-antialiased">
+            <div>
+                <Navbar />
+                <Footer />
+            </div>
+        </main>
+    );
 }
