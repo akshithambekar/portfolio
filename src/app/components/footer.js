53af58a269c6263f43fd23d0818762ea65228c19
Akshith Ambekar <123666607+akshithambekar@users.noreply.github.com>
2025-12-20 16:39:01 -0500

    portfolio navbar rework

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -2,8 +2,8 @@ import { PiCopyrightLight } from "react-icons/pi";
 
 export default function Footer() {
     return (
-        <div className="max-w-[1260px] flex justify-center items-center min-h-[60px] text-custom_gray px-4 sm:px-8 md:px-16 lg:px-24 xl:px-44 pt-12 sm:pt-24 pb-8 sm:pb-10 subpixel-antialiased mx-auto">
-            <div className="flex flex-row items-center space-x-1 text-xs sm:text-sm text-center">
+        <div className="max-w-[860px] flex justify-center items-center min-h-[60px] text-custom_gray px-4 sm:px-8 md:px-16 lg:px-24 xl:px-44 pt-12 sm:pt-24 pb-8 sm:pb-10 subpixel-antialiased mx-auto">
+            <div className="flex flex-row items-center space-x-1 text-xs sm:text-sm text-center opacity-80">
                 <PiCopyrightLight className="flex-shrink-0" />
                 <p>
                     2025 Akshith Ambekar. All rights reserved. Last updated on
---
+++ src/app/projects/page.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+export default function Home() {
+    return (
+        <main className="subpixel-antialiased">
+            <div>
+                <Navbar />
+                <Footer />
+            </div>
+        </main>
+    );
+}
