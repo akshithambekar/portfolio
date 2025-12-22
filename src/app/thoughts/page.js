c1aca90680ad9daaebebca80e0bf1994a2c88061
Akshith Ambekar <123666607+akshithambekar@users.noreply.github.com>
2025-12-21 22:08:52 -0500

    thoughts page

---
+++ src/app/thoughts/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <main className="subpixel-antialiased">
+            <div className="max-w-[512px] justify-center items-center flex flex-col mx-auto">
+                <p className="text-xl font-thin text-justify py-5">
+                    coming soon. a mix of essays and thoughts about ai, building
+                    products, or just anything i find interesting.
+                </p>
+            </div>
+        </main>
+    );
+}
